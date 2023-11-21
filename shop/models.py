c5a78f4e01d4fb238ce6ae3e4061e75eb4f1459c
Katrin Hanikel <katrin.hanikel@gmail.com>
2023-11-21 15:07:48 +0100

    warenkorb

---
+++ shop/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Kunde(models.Model):
+    benutzer = models.OneToOneField(User, verbose_name=("Benutzer"), on_delete=models.RESTRICT, null=True, blank=True)
+    name = models.CharField(("Name"), max_length=200, null=True, blank=True)
+    email = models.CharField(("E-Mail"), max_length=50, null=True, blank=True)
+
+    class Meta:
+        verbose_name = ("Kunde")
+        verbose_name_plural = ("KundeN")
+
+    def _str_(self):
+        return f"{self.benutzer}/{self.name}"
+
+    def get_absolute_url(self):
+        return reverse("Kunde_detail", kwargs={"pk": self.pk})
